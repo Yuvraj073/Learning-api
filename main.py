c3169ae64963ff871abca0f7d8a430e9dd62e53b
Yuvraj073 <yuvrajnaruka40@gmail.com>
2026-06-24 22:04:26 +0530

    updated schema and main to connect database

--- main.py
+++ main.py
@@ -1,31 +1,27 @@
-from fastapi import FastAPI , Request , HTTPException , status
+from fastapi import FastAPI , Request , HTTPException , status , Depends
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from starlette.exceptions import HTTPException as StarletteHTTPException
 from schemas import PostCreate, PostResponse
+from typing import Annotated
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+import models
+from database import Base, engine, get_db
+
+Base.metabata.create_all(bind=engine)
+
 
 app = FastAPI()
+
 app.mount("/static", StaticFiles(directory="static"), name="static") 
+app.mount("/media", StaticFiles(directory="media"), name="media")
+
 templates = Jinja2Templates(directory="templates")
 
-posts: list[dict] = [
-    {
-        "id": 1,
-        "author": "Corey Schafer",
-        "title": "FastAPI is Awesome",
-        "content": "This framework is really easy to use and super fast.",
-        "date_posted": "April 20, 2025",
-    },
-    {
-        "id": 2,
-        "author": "Jane Doe",
-        "title": "Python is Great for Web Development",
-        "content": "Python is a great language for web development, and FastAPI makes it even better.",
-        "date_posted": "April 21, 2025",
-    },
-]
+
 
 @app.get("/",include_in_schema=False, name="home")
 @app.get("/posts",include_in_schema=False, name="posts")
